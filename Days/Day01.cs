9f6ace2e15ad8dd6166274619174dfb00ee28694
Gareth Spring <gareth.spring@dunnhumby.com>
2022-12-01 09:12:58 +0000

    Day 1

---
+++ Days/Day01.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2022.Days;
+
+public class Day01
+{
+    public int Day01Part01(List<Elf> input)
+    {
+        return input.Max(e => e.Calories.Sum());
+    }
+    
+    public int Day01Part02(List<Elf> input)
+    {
+        return ParseMax(input) + ParseMax(input) + ParseMax(input);
+    }
+
+    private int ParseMax(List<Elf> input)
+    {
+        int maxId = -1;
+        int maxCalories = 0;
+        
+        foreach (Elf elf in input)
+        {
+            var totalCals = elf.Calories.Sum();
+
+            if (totalCals <= maxCalories)
+                continue;
+            
+            maxCalories = totalCals;
+            maxId = elf.Id;
+        }
+        
+        input.Remove(input.Single(x => x.Id == maxId));
+
+        return maxCalories;
+    }
+}
+
+public class Elf
+{
+    public int Id { get; init; }
+    
+    public List<int>? Calories { get; init; }
+}
---
+++ Tests/Day01Tests.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCode2022.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day01Tests
+    {
+        [Fact]
+        public void Day01Part1()
+        {
+            var day = new Day01();
+            var input = ReadInput();
+            
+            var result = day.Day01Part01(input);
+            
+            result.Should().Be(69206);
+        }
+        
+        [Fact]
+        public void Day01Part2()
+        {
+            var day = new Day01();
+            var input = ReadInput();
+            
+            var result = day.Day01Part02(input);
+
+            result.Should().Be(197400);
+        }
+
+        private static List<Elf> ReadInput()
+        {
+            var input = File.ReadAllLines(Environment.CurrentDirectory + "/Input/Day01Input.txt");
+
+            var result = new List<Elf>();
+            var elf = new Elf { Calories = new List<int>(), Id = 0 };
+
+            int id = 0;
+            
+            foreach (string s in input)
+            {
+                if (string.IsNullOrEmpty(s.Trim()))
+                {
+                    result.Add(elf);
+                    id++;
+                    elf = new Elf
+                    {
+                        Calories = new List<int>(),
+                        Id = id
+                    };
+                }
+                else 
+                {
+                    elf.Calories.Add(Convert.ToInt32(s));
+                }
+            }
+            
+            result.Add(elf);
+
+            return result;
+        }
+    }
+}
