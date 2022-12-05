a32d21d3f5cb3afd68b826031de9ffb31c7f0b86
Gareth Spring <gareth.spring@dunnhumby.com>
2022-12-05 09:22:36 +0000

    Day 5

---
+++ Days/Day05.cs
@@ -0,0 +1,65 @@
+﻿namespace AdventOfCode2022.Days
+{
+    public class Day05
+    {
+        public string Day05Part01(Crane state, List<Move> moves)
+        {
+            foreach (Move m in moves)
+            {
+                for (int i = 0; i < m.Quantity; i++)
+                {
+                    string temp = state.Crates[m.Source - 1].Pop();
+                    state.Crates[m.Destination -1].Push(temp);
+                }
+            }
+
+            var result = string.Empty;
+            foreach (var t in state.Crates)
+            {
+                result += t.Peek();
+            }
+            
+            return result;
+        }
+        
+        public string Day05Part02(Crane state, List<Move> moves)
+        {
+            foreach (Move m in moves)
+            {
+                var tempStack = new Stack<string>();
+                
+                for (int i = 0; i < m.Quantity; i++)
+                {
+                    tempStack.Push(state.Crates[m.Source - 1].Pop());
+                }
+                
+                for (int i = 0; i < m.Quantity; i++)
+                {
+                    state.Crates[m.Destination - 1].Push(tempStack.Pop());
+                }
+            }
+
+            var result = string.Empty;
+            foreach (var t in state.Crates)
+            {
+                result += t.Peek();
+            }
+            
+            return result;
+        }
+    }
+
+    public class Crane
+    {
+        public List<Stack<string>> Crates { get; set; }
+    }
+
+    public class Move
+    {
+        public int Quantity { get; init; }
+        
+        public int Source { get; init; }
+        
+        public int Destination { get; init; }
+    }
+}
---
+++ Tests/Day05Tests.cs
@@ -0,0 +1,91 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode2022.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day05Tests
+    {
+        private Crane crane;
+
+        [Fact]
+        public void Day05Part1Test()
+        {
+            Setup();
+            
+            var day = new Day05();
+            var moves = ReadInput();
+
+            var result = day.Day05Part01(crane, moves);
+            
+            result.Should().Be("RTGWZTHLD");
+        }
+        
+        [Fact]
+        public void Day05Part2Test()
+        {
+            Setup();
+            
+            var day = new Day05();
+            var moves = ReadInput();
+
+            var result = day.Day05Part02(crane, moves);
+            
+            result.Should().Be("STHGRZZFR");
+        }
+
+        private void PushStack(string input, Crane crane)
+        {
+            var s = new Stack<string>();
+            foreach (char c in input)
+            {
+               s.Push(c.ToString());
+            }
+            
+            crane.Crates.Add(s);
+        }
+
+        private void Setup()
+        {
+            crane = new Crane
+            {
+                Crates = new List<Stack<string>>()
+            };
+            
+            PushStack("NCRTMZP", crane);
+            PushStack("DNTSBZ", crane);
+            PushStack("MHQRFCTG", crane);
+            PushStack("GRZ", crane);
+            PushStack("ZNRH", crane);
+            PushStack("FHSWPZLD", crane);
+            PushStack("WDZRCGM", crane);
+            PushStack("SJFLHWZQ", crane);
+            PushStack("SQPWN", crane);
+        }
+
+        private static List<Move> ReadInput()
+        {
+            var moves = new List<Move>();
+            var input = File.ReadAllLines(Environment.CurrentDirectory + "/Input/Day05Input.txt");
+
+            var regex = new Regex(@"move\s(\d{1,2})\sfrom\s(\d)\sto\s(\d)");
+            
+            foreach (string line in input)
+            {
+                Match? matches = regex.Match(line);
+                
+                var move = new Move
+                {
+                    Quantity = Convert.ToInt32(matches.Groups[1].Value),
+                    Source = Convert.ToInt32(matches.Groups[2].Value),
+                    Destination = Convert.ToInt32(matches.Groups[3].Value)
+                };
+                
+                moves.Add(move);
+            }
+            
+            return moves;
+        }
+    }
+}
