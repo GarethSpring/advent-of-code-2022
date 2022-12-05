772052d78fd53b643910e011fa53bff6e5861135
Gareth Spring <gareth.spring@dunnhumby.com>
2022-12-05 15:04:16 +0000

    Day 4

---
+++ Days/Day04.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2022.Days
+{
+    public class Day04
+    {
+        public int Day04Part01(List<ElfPair> input)
+        {
+            return input.Select(ep =>
+                ep.R1.Start.Value >= ep.R2.Start.Value && ep.R1.End.Value <= ep.R2.End.Value
+                || ep.R2.Start.Value >= ep.R1.Start.Value && ep.R2.End.Value <= ep.R1.End.Value).Count(p => p);
+        }
+        
+        public int Day04Part02(List<ElfPair> input)
+        {
+            return input.Select(ep => 
+                ep.R1.End.Value >= ep.R2.Start.Value && ep.R1.End.Value <= ep.R2.End.Value
+                ||  ep.R2.End.Value >= ep.R1.Start.Value && ep.R2.End.Value <= ep.R1.End.Value).Count(p => p);
+        }
+    }
+
+    public class ElfPair
+    {
+        public Range R1 { get; init; }
+        
+        public Range R2 { get; init; }
+    }
+}
---
+++ Tests/Day04Tests.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode2022.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void Day04Part1Test()
+        {
+            var day = new Day04();
+            var input = ReadInput();
+            
+            var result = day.Day04Part01(input);
+            
+            result.Should().Be(513);
+        }
+        
+        [Fact]
+        public void Day04Part2Test()
+        {
+            var day = new Day04();
+            var input = ReadInput();
+            
+            var result = day.Day04Part02(input);
+            
+            result.Should().Be(878);
+        }
+
+        private static List<ElfPair> ReadInput()
+        {
+            var result = new List<ElfPair>();
+            
+            var input = File.ReadAllLines(Environment.CurrentDirectory + "/Input/Day04Input.txt");
+
+            foreach (string line in input)
+            {
+                var regex = new Regex(@"(\d{1,2})-(\d{1,2}),(\d{1,2})-(\d{1,2})");
+                
+                Match? matches = regex.Match(line);
+
+                var ep = new ElfPair
+                {
+                    R1 = Convert.ToInt32(matches.Groups[1].Value)..Convert.ToInt32(matches.Groups[2].Value),
+                    R2 = Convert.ToInt32(matches.Groups[3].Value)..Convert.ToInt32(matches.Groups[4].Value),
+                };
+                
+                result.Add(ep);
+            }
+
+            return result;
+        }
+    }
+}
