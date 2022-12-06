79a36f2f29ec7d136077f0223068a968e3a8cddc
Gareth Spring <gareth.spring@dunnhumby.com>
2022-12-06 10:07:43 +0000

    Day 6

---
+++ Days/Day06.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2022.Days
+{
+    public class Day06
+    {
+        private int markerLength = 0;
+        
+        public int Day06Logic(string input, int markerLength)
+        {
+            this.markerLength = markerLength;
+            var marker = new List<char>();
+            var result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (CheckMarker(marker, input[i]))
+                {
+                    result = i + 1;
+                    break;
+                }
+                
+            }
+            
+            return result;
+        }
+
+        private bool CheckMarker(List<char> marker, char c)
+        {
+            if (marker.Count < this.markerLength)
+            {
+                marker.Add(c);
+                return false;
+            }
+            
+            marker.RemoveAt(0);
+            marker.Add(c);
+            
+            return marker.Distinct().Count() == this.markerLength;
+        }
+        
+    }
+}
---
+++ Tests/Day06Tests.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2022.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day06Tests
+    {
+        [Fact]
+        public void Day06Part1Test()
+        {
+            var day = new Day06();
+            var input = ReadInput();
+
+            var result = day.Day06Logic(input, 4);
+            
+            result.Should().Be(1198);
+        }
+        
+        [Fact]
+        public void Day06Part2Test()
+        {
+            var day = new Day06();
+            var input = ReadInput();
+
+            var result = day.Day06Logic(input, 14);
+            
+            result.Should().Be(3120);
+        }
+
+        private static string ReadInput()
+        {
+            var input = File.ReadAllLines(Environment.CurrentDirectory + "/Input/Day06Input.txt");
+
+            return input.First();
+        }
+    }
+}
