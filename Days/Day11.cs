96031fe318db74158b1f7fa76f9f73e4d1e9ae08
Gareth Spring <gareth.spring@gmail.com>
2022-12-17 15:56:22 +0000

    Day 11

---
+++ Days/Day11.cs
@@ -0,0 +1,91 @@
+﻿namespace AdventOfCode2022.Days
+{
+    public class Day11
+    {
+        private List<Monkey> _monkeys;
+        
+        public long Part1(List<Monkey> monkeys)
+        {
+            _monkeys = monkeys;
+            for (int i = 0; i < 20; i++)
+            {
+                PlayRound(true);
+            }
+
+            var orderByDescending = _monkeys.OrderByDescending(m => m.Inspections);
+            var result = orderByDescending.Take(1).First().Inspections * orderByDescending.Skip(1).Take(1).First().Inspections;
+            return result;
+        }
+        
+        public long Part2(List<Monkey> monkeys)
+        {
+            _monkeys = monkeys;
+            
+            // Least Common Multiple 
+            long lcm = 13 * 7 * 3 * 19 * 5 *  2 * 11 * 17;
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                PlayRound(false, lcm);
+            }
+
+            var orderByDescending = _monkeys.OrderByDescending(m => m.Inspections);
+            var result = orderByDescending.Take(1).First().Inspections * orderByDescending.Skip(1).Take(1).First().Inspections;
+            return result;
+        }
+
+        private void PlayRound(bool divide, long lcm = 0)
+        {
+            for (int m = 0; m < _monkeys.Count; m++)
+            {
+                for (int itemIndex = 0; itemIndex < _monkeys[m].StartingItems.Count; itemIndex++)
+                {
+                    _monkeys[m].Inspections++;
+                    _monkeys[m].StartingItems[itemIndex] = m switch
+                    {
+                        0 => _monkeys[m].StartingItems[itemIndex] * 11,
+                        1 => _monkeys[m].StartingItems[itemIndex] + 1,
+                        2 => _monkeys[m].StartingItems[itemIndex] * _monkeys[m].StartingItems[itemIndex],
+                        3 => _monkeys[m].StartingItems[itemIndex] + 2,
+                        4 => _monkeys[m].StartingItems[itemIndex] + 6,
+                        5 => _monkeys[m].StartingItems[itemIndex] + 7,
+                        6 => _monkeys[m].StartingItems[itemIndex] * 7,
+                        7 => _monkeys[m].StartingItems[itemIndex] + 8,
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+                    
+                    _monkeys[m].StartingItems[itemIndex] = divide 
+                        ? Convert.ToInt64(Math.Floor((double) _monkeys[m].StartingItems[itemIndex] / 3))
+                        : _monkeys[m].StartingItems[itemIndex] %= lcm;
+
+                    // Throw
+                    if (_monkeys[m].StartingItems[itemIndex] % _monkeys[m].DivisibleBy == 0)
+                    {
+                        _monkeys[_monkeys[m].TrueThrowIndex].StartingItems.Add(_monkeys[m].StartingItems[itemIndex]);
+                    }
+                    else
+                    {
+                        _monkeys[_monkeys[m].FalseThrowIndex].StartingItems.Add(_monkeys[m].StartingItems[itemIndex]);
+                    }
+                }
+
+                _monkeys[m].StartingItems = new List<long>();
+            }
+        }
+    }   
+
+    public class Monkey
+    {
+        public int Id { get; init; }
+        
+        public List<long> StartingItems { get; set; }
+        
+        public int TrueThrowIndex { get; init; }
+        
+        public int FalseThrowIndex { get; init; }
+
+        public int DivisibleBy { get; init; }
+        
+        public long Inspections { get; set; }
+    }
+}
---
+++ Tests/Day11Tests.cs
@@ -0,0 +1,79 @@
+﻿using AdventOfCode2022.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day11Tests
+    {
+    
+            [Fact]
+            public void Day11Part1Test()
+            {
+                var day = new Day11();
+                
+                var result = day.Part1(InitMonkeys());
+                
+                result.Should().Be(66802);
+            }
+            
+            [Fact]
+            public void Day11Part2Test()
+            {
+                var day = new Day11();
+                
+                var result = day.Part2(InitMonkeys());
+
+                result.Should().Be(21800916620);
+            }
+            
+            private List<Monkey> InitMonkeys()
+            {
+                var monkeys = new List<Monkey>
+                {
+                    new Monkey
+                    {
+                        Id = 0, StartingItems = new List<long> {71, 56, 50, 73},
+                        DivisibleBy = 13, TrueThrowIndex = 1,  FalseThrowIndex = 7
+                    },
+                    new Monkey
+                    {
+                        Id = 1, StartingItems = new List<long> {70, 89, 82},
+                        DivisibleBy = 7, TrueThrowIndex = 3,  FalseThrowIndex = 6
+                    },
+                    new Monkey
+                    {
+                        Id = 2, StartingItems = new List<long> {52, 95},
+                        DivisibleBy = 3, TrueThrowIndex = 5,  FalseThrowIndex = 4
+                    },
+                    new Monkey
+                    {
+                        Id = 3, StartingItems = new List<long> {94, 64, 69, 87, 70},
+                        DivisibleBy = 19, TrueThrowIndex = 2,  FalseThrowIndex = 6
+                    },
+                    new Monkey
+                    {
+                        Id = 4, StartingItems = new List<long> {98, 72, 98, 53, 97, 51},
+                        DivisibleBy = 5, TrueThrowIndex = 0,  FalseThrowIndex = 5
+                    },
+                    new Monkey
+                    {
+                        Id = 5, StartingItems = new List<long> {79},
+                        DivisibleBy = 2, TrueThrowIndex = 7,  FalseThrowIndex = 0
+                    },
+                    new Monkey
+                    {
+                        Id = 6, StartingItems = new List<long> {77, 55, 63, 93, 66, 90, 88, 71},
+                        DivisibleBy = 11, TrueThrowIndex = 2,  FalseThrowIndex = 4
+                    },
+                    new Monkey
+                    {
+                        Id = 7, StartingItems = new List<long> {54, 97, 87, 70, 59, 82, 59},
+                        DivisibleBy = 17, TrueThrowIndex = 1,  FalseThrowIndex = 3
+                    }
+                };
+
+                return monkeys;
+            }
+    }
+}
